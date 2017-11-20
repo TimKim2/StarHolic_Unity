88dc02ea50542460a4e61873206ee7c1f846c326
전민규 <poa9253@naver.com>
2017-11-20 14:04:09 +0900

    MG is my project

---
+++ MG/Assets/Script/GameObject/Enemy/ZombieMove.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieMove : MonoBehaviour {
+
+	public float moveSpeed = 3.0f;
+	public Vector3 target;
+	public float lifetime = 20.0f;
+	Rigidbody2D rb;
+	// Use this for initialization
+	void Start () {
+		rb = gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate(){
+		//Destroy (gameObject, lifetime);
+		target = GameObject.FindGameObjectWithTag ("Player").transform.position;
+		Vector3 vector = Vector3.Normalize(target - this.transform.position);
+		//Vector3 vector = Vector3.Normalize(target - this.transform.position);
+		//Normalize를 지우면 꿀 잼 
+		this.rb.MovePosition(transform.position + vector * Time.fixedDeltaTime * moveSpeed);
+	}
+	private void OnEnable(){
+		Debug.Log ("Spawn?");
+		Random.InitState ((int)Time.realtimeSinceStartup);
+		float randomXPos = Random.Range(5.5f, -5.5f);
+		float randomYPos = Random.Range(10.0f, -10.0f);
+		//target = GameObject.FindGameObjectWithTag ("Player").transform.position;
+		this.transform.position=new Vector3(randomXPos, randomYPos, 0.0f);
+		
+	}
+}
---
+++ MG/Assets/Script/GameObject/ItemBall/ItemBall.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemBall : MonoBehaviour {
+
+	public Rigidbody2D rb;
+	public Vector3 player;
+	public bool death = false;
+	public float lifetime= 7.0f;
+	// Use this for initialization
+	void Start () {
+		rb = gameObject.GetComponent<Rigidbody2D> ();
+		//anim = gameObject.GetComponent<Animation> ();
+	}
+	void Update(){
+		//Destroy(gameObject, lifetime);
+	}
+	void OnCollisionEnter2D(Collision2D other){
+		if(other.gameObject.tag.Equals("Enemy")){
+			Destroy(other.gameObject);
+			Debug.Log ("ball");
+		}
+	}
+	private void OnEnable(){
+		Vector3 NormalizedVector = Vector3.Normalize(TouchPanel.Instance.deltaMousePos);
+		player = GameObject.FindGameObjectWithTag ("Player").transform.position;
+		this.rb.transform.position = player + 3 * NormalizedVector ;
+	}
+}
